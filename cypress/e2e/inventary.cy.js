77fd52be75867b2ca87fee06445b729d99ef2e5a
Evelyn Navazio <evelynnavazio@gmail.com>
2026-05-27 21:56:54 -0300

    Inventory tests area

--- cypress/e2e/inventary.cy.js
+++ cypress/e2e/inventary.cy.js
@@ -1,13 +1,39 @@
-describe('Inventory - Sauce Demo', () =>{
+describe('Inventario - SauceDemo', () => {
 
-beforEach(() => {
-    cy.visit('https://saucedemo.com/')
-})
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/')
 
-it('nombre', ()=>{
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
 
-}
+        cy.url().should('include', '/inventory.html')
+    })
 
-)
+    it('TC 6 - Verificar cantidad de productos en inventario', () => {
+
+        cy.viewport(1366, 768)
+
+        cy.get('.inventory_item').as('productos')
+
+        cy.get('@productos')
+        .should('have.length', 6)
+
+    })
+
+    it('TC 8 - Ordenar productos por precio menor a mayor', () => {
+
+        cy.get('[data-test="product-sort-container"]')
+        .select('lohi')
+
+        cy.get('.inventory_item_price')
+        .first()
+        .should('contain', '$7.99')
+
+        cy.get('.inventory_item_name')
+        .first()
+        .should('contain', 'Sauce Labs Onesie')
+
+    })
 
 })
