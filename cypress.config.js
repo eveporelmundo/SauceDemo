4035982c71ee8046b66d8384d6fc9f11110c0149
Evelyn Navazio <evelynnavazio@gmail.com>
2026-06-22 16:36:18 -0300

    install mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,21 @@
+
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+  reporter: "cypress-mochawesome-reporter",
+
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "SauceDemo Cypress Test Suite Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
--- cypress/e2e/testacademy.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Grupo de Pruebas', function(){
-it('Prueba 1',function(){
-cy.visit('https://www.saucedemo.com//');
-cy.contains( 'Carrito' ).click();
-})
-})
